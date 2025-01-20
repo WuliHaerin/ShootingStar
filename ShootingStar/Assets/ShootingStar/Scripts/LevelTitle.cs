1ff7a51ab54fc31742fe9ee414acaf8f6a9886fe
CZY <chenzhengyoung@gmail.com>
2025-01-20 15:03:49 +0800

    1.0

---
+++ ShootingStar/Assets/ShootingStar/Scripts/LevelTitle.cs
@@ -0,0 +1,21 @@
+﻿/*
+LevelTitle.cs
+Version 1.0
+
+Sets the Title of the Level
+*/
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class LevelTitle : MonoBehaviour {
+
+	void Awake()
+	{
+		int LevelNum = LevelData.getCurrentLevelNum();
+		GetComponent<TMP_Text>().text = "关卡 " + LevelNum.ToString();
+	}
+
+}
